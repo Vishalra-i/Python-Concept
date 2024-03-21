d82975ae369b900575a48b169fb5ced46131a14a
Vishal Rai <135126334+Vishalra-i@users.noreply.github.com>
2024-03-21 01:01:07 +0000

    slice

---
+++ hi.js
@@ -0,0 +1,5 @@
+console.log("hii")
+
+let l = true
+
+l.map(kjfd)
---
+++ slice.py
@@ -0,0 +1,8 @@
+h1 = [1,2,3]
+h2 = h1[:]
+h1[0] = 22
+
+
+print(h1)  # Output: [22, 2, 3]
+print(h2)  # Output: [1, 2, 3]
+print("hii")
